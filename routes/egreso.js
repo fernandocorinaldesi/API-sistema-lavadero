b5f97d51d4a7409da640aa8ab8432e9c891191ee
Fernando Corinaldesi <ferchucorinaldesi@gmail.com>
2021-11-11 12:21:33 -0300

    Se agregaron las rutas de egreso

---
+++ routes/egreso.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const router = express.Router()
+const egresoController = require('../controllers/egresoController')
+const multer = require('../utils/multer/multerRouteModule')
+const tokenAuth = require('../utils/tokenAuth')
+const validator = require("./validator")
+
+// Se asigna el controlador correspondiente a cada ruta
+
+/*router.get('/',
+    // tokenAuth,
+    ordenTrabajoController.getAll
+)
+
+router.get('/:id',
+    // tokenAuth,
+    //publicacionesController.findById
+)
+router.get('/detalles/:id',
+    // tokenAuth,
+    ordenTrabajoController.getDetalles
+    //publicacionesController.findById
+)*/
+router.post('/',
+    //tokenAuth,
+   // multer.upload,
+   // validator.pubValidationRules(),
+    //validator.pubValidate,
+    egresoController.addEgreso
+)
+
+/*router.delete('/:id',
+   // tokenAuth,
+   compraController.del
+)
+
+router.put(
+    '/:id',
+    compraController.updateCompra
+  //  tokenAuth,
+ //   multer.upload,
+ //   validator.pubValidationRules(),
+   // validator.pubValidate,publicacionesController.editar
+)
+*/
+module.exports = router
