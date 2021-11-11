03c1b92c67ac613e49dfd672306b555bc61a7ca0
Fernando Corinaldesi <ferchucorinaldesi@gmail.com>
2021-11-11 12:23:44 -0300

    Se agregaron servicios para el cliente

--- services/clienteServices.js
+++ services/clienteServices.js
@@ -1,21 +1,44 @@
 const Cliente = require("../models/cliente");
 
 exports.getAllClients = async () => {
-
-
     resultado = await Cliente.findAll()
-  
-  
     return resultado
   }
-
-  
+ 
   exports.getClientByDni = async (dni)  =>{
     resultado = await Cliente.findOne({ where: { dni:dni  } })
     return resultado
   }
 
+  exports.getClientByid = async (id)  =>{
+    resultado = await Cliente.findOne({ where: { id:id  } })
+    return resultado
+  }
+
   exports.add = async (body)  =>{
     
+    resultado = await Cliente.create({
+      nombre: body.nombre,
+      apellido: body.apellido,
+      dni: body.dni,
+      email: body.email,
+      direccion: body.direccion,
+      telefono: body.telefono
+    })
+    return resultado
+  }
+
+  exports.edit = async (body,id)  =>{
+    
+    resultado = await Cliente.update({
+      nombre: body.nombre,
+      apellido: body.apellido,
+      dni: body.dni,
+      email: body.email,
+      direccion: body.direccion,
+      localidad:body.localidad,
+      provincia:body.provincia,
+      telefono: body.telefono
+    },{ where: { id: id }})
     return resultado
   }
